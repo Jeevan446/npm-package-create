2fb5498013fee3de2ea5538dec707580f26533f1
user.name:"Jeevan neupane <user.email:"neupanejeevan446@gmail.com>
2025-02-16 07:31:56 +0545

    first commit

---
+++ index.cjs
@@ -0,0 +1,10 @@
+const maths={
+    greet:function(){
+        console.log("Hello dai");
+    },
+    bye:function (){
+        console.log("Bye Bye dai")
+    }
+}
+
+module.exports=maths;
---
+++ index.mjs
@@ -0,0 +1,10 @@
+// ES6 export
+export const myObject = {
+    greet: function() {
+      console.log("Hello dai from ES6");
+    },
+    bye: function() {
+      console.log("Bye Bye dai from ES6");
+    }
+  };
+  
